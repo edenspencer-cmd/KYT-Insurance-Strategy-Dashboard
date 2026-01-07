2fb26b47691ea21a3173cd87f7f1d56d66b0c826
edenspencer-cmd <eden.spencer@knowyourtrade.com.au>
2026-01-07 13:57:07 +1000

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,11 @@
+
+'use client';
+
+export default function Page() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>KYT Growth Dashboard</h1>
+      <p>Paste the dashboard component code here.</p>
+    </div>
+  );
+}
